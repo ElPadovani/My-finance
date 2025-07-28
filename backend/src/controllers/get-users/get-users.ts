b2dcae35b9b02546bcf8ac035868f6dff1ed417e
ElPadovan <ebpadovani@gmail.com>
2025-07-28 11:04:39 -0300

    feat: add getUserController

---
+++ backend/src/controllers/get-users/get-users.ts
@@ -0,0 +1,24 @@
+import { IGetUsersController, IGetUsersRepository } from "./protocols";
+
+export class getUsersController implements IGetUsersController {
+  constructor(private readonly getUsersRepository: IGetUsersRepository) {}
+
+  async handle() {
+    try {
+      // validar req
+      // direcionar chamada pro repository
+      const users = await this.getUsersRepository.getUsers();
+      
+      return {
+        statusCode: 200,
+        body: users
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "SomethingWentWrong"
+      }
+    }
+
+  }
+}
---
+++ backend/src/models/user.ts
@@ -0,0 +1,6 @@
+export interface User {
+  name: string;
+  email: string;
+  passwords: string;
+  creation_date: string;
+}
