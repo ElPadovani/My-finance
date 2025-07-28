97b745c4c99a32ffbcb8da463eb58d8aea7acaaa
ElPadovan <ebpadovani@gmail.com>
2025-07-28 10:48:34 -0300

    feat: add express initial server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world")
+import express from 'express';
+import { config } from 'dotenv';
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`listening on port: ${port}!`));
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+})
