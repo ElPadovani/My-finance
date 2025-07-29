f2a8347c010109d03827b62ac0af4597490d2cb0
ElPadovan <ebpadovani@gmail.com>
2025-07-29 07:48:41 -0300

    feat: add MongoUpdateUserRepository

---
+++ backend/src/controllers/update-user/protocols.ts
@@ -0,0 +1,11 @@
+import { User } from "../../models/user";
+
+export interface UpdateUserParams {
+  name?: string;
+  password?: string;
+  email?: string;
+};
+
+export interface IUpdateUserRepository {
+  updateUser(id:string, params: UpdateUserParams): Promise<User>;
+};
