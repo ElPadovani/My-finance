00b6811d53dad3018bdab01fc07cce8a39ac673a
ElPadovan <ebpadovani@gmail.com>
2025-07-29 11:47:35 -0300

    feat: add getExpenses route

--- backend/src/routes/expense-routes.ts
+++ backend/src/routes/expense-routes.ts
@@ -1,8 +1,20 @@
 import { Router } from "express";
 import { MongoCreateExpenseRepository } from "../repositories/create-expense/mongo-create-expense";
 import { CreateExpenseController } from "../controllers/create-expense/create-expense";
+import { GetExpensesController } from "../controllers/get-expenses/get-expenses";
+import { MongoGetExpensesRepository } from "../repositories/get-expenses/mongo-get-expenses";
 
 const router = Router();
+
+router.get("/users", async (req, res) => {
+  const mongoGetExpensesRepository = new MongoGetExpensesRepository();
+
+  const getExpensesController = new GetExpensesController(mongoGetExpensesRepository);
+  
+  const { body, statusCode } = await getExpensesController.handle();
+  
+  res.status(statusCode).send(body);
+});
 
 router.post("/expenses", async (req, res) => {
   const mongoCreateExpenseRepository = new MongoCreateExpenseRepository();
