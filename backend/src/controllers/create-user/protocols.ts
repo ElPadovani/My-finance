fe118c13786e9d84ee250ecbbe378af3b63605cc
ElPadovani <ebpadovani@gmail.com>
2025-07-28 22:10:26 -0300

    Teste de commit

--- backend/src/controllers/create-user/protocols.ts
+++ backend/src/controllers/create-user/protocols.ts
@@ -5,14 +5,14 @@ export interface ICreateUserController {
   handle(
     httpRequest: HttpRequest<CreateUserParams>
   ): Promise<HttpResponse<User>>
-}
+};
 
 export interface CreateUserParams {
   name: string;
   email: string;
   password: string;
-}
+};
 
 export interface ICreateUserRepository {
   createUser(params: CreateUserParams): Promise<User>;
-}
+};
