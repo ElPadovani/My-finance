20d6a8c9e9f9129b3454bb823e721f9d3d1640e7
ElPadovani <ebpadovani@gmail.com>
2025-09-23 14:42:10 -0300

    feat: add new field start_date and end_Date to get some expenses

--- backend/src/controllers/get/get-user-expenses/protocols.ts
+++ backend/src/controllers/get/get-user-expenses/protocols.ts
@@ -4,6 +4,8 @@ export interface GetUserExpenseParams {
   description?: string;
   category?: string;
   expense_date?: Date;
+  start_date?: string;
+  end_date?: string;
 }
 
 export interface IGetUserExpenseRepository {
--- backend/src/repositories/get/get-user-expenses/mongo-get-user-expenses.ts
+++ backend/src/repositories/get/get-user-expenses/mongo-get-user-expenses.ts
@@ -28,6 +28,16 @@ export class MongoGetUserExpenseRepository implements IGetUserExpenseRepository 
       query.expense_date = { $gte: start, $lte: end };
     }
 
+    if(expenseParams?.start_date && expenseParams?.end_date) {
+      const start = new Date(expenseParams.start_date);
+      start.setHours(0, 0, 0, 0);
+
+      const end = new Date(expenseParams.end_date);
+      end.setHours(23, 59, 59, 999);
+
+      query.expense_date = { $gte: start, $lte: end };
+    }
+
     const expenses = await MongoClient.db
       .collection<MongoExpense>("expenses")
       .find(query)
